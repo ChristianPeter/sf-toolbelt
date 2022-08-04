cada5e7af269bb69b53a4bced6dc0b2559a9fcda
PteC <ptec@viessmann.com>
2022-08-04 15:20:49 +0200

    first tool: to apex string conversion

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,33 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sftoolbelt.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('sftoolbelt.toApexString', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SFToolbelt!');
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor && editor?.selections.length > 0){ 
+			const selections = editor.selections;
+			const selectedText = editor?.document.getText(editor.selection) as string;
+
+			const splitted = selectedText.split('\n');
+
+			let newContent = splitted
+				.filter(row => row.trim().length > 0)
+				.map(row => {
+					return row.replace('\'', '\\\'');
+				})
+				.map(row => {
+				return "'" + row + "' +";
+			}).join('\n');
+
+			newContent = newContent.substring(0, newContent.length-2);
+
+			vscode.window.activeTextEditor?.edit(editBuilder => {
+				editBuilder.replace(selections[0], newContent);
+			});
+		}
+		
 	});
 
 	context.subscriptions.push(disposable);
