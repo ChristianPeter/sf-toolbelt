0d4e1741f1ee9f582725ef45d9435977a030ef0e
PteC <ptec@viessmann.com>
2022-08-05 08:56:51 +0200

    IN-Converter added

--- src/extension.ts
+++ src/extension.ts
@@ -14,35 +14,102 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('sftoolbelt.toApexString', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+
+
 		const editor = vscode.window.activeTextEditor;
+		const selections = editor?.selections;
 
-		if (editor && editor?.selections.length > 0){ 
-			const selections = editor.selections;
-			const selectedText = editor?.document.getText(editor.selection) as string;
+		if (selections == null || selections.length === 0) return undefined;
 
-			const splitted = selectedText.split('\n');
+		return vscode.window.activeTextEditor?.edit(editBuilder => {
+			for (let idx = 0; idx < selections.length; idx++) {
+				const currentText = editor?.document.getText(selections[idx]) || '';
+				const splitted = currentText.split('\n');
+				let newContent = splitted
+					.filter(row => row.trim().length > 0)
+					.map(row => {
+						return row.replace(/\'/gi, '\\\'');
+					})
+					.map(row => {
+					return "'" + row + "' +";
+				}).join('\n');
 
-			let newContent = splitted
-				.filter(row => row.trim().length > 0)
-				.map(row => {
-					return row.replace('\'', '\\\'');
-				})
-				.map(row => {
-				return "'" + row + "' +";
-			}).join('\n');
-
-			newContent = newContent.substring(0, newContent.length-2);
-
-			vscode.window.activeTextEditor?.edit(editBuilder => {
-				editBuilder.replace(selections[0], newContent);
-			});
-		}
+				newContent = newContent.substring(0, newContent.length-2);
+				editBuilder.replace(selections[idx], newContent);
+			}
+			
+		});
 		
 	});
 
+	let disposable2 = vscode.commands.registerCommand('sftoolbelt.inConverter', () => {
+		const editor = vscode.window.activeTextEditor;
+		const selections = editor?.selections;
+		const maxLen = 19000; // TODO: Make it a query box param
+		
+		if (selections == null || selections.length === 0) return undefined;
+
+		return vscode.window.activeTextEditor?.edit(editBuilder => {
+			for (let idx = 0; idx < selections.length; idx++) {
+				const currentText = editor?.document.getText(selections[idx]) || '';
+				const splitted = currentText.split('\n');
+
+				const lenOfInput = currentText.length;
+				const numberOfRows = splitted.length;
+				const avgLineLen = numberOfRows > 0 ? lenOfInput / numberOfRows : 0;
+
+
+				let cleanedUpRows = splitted
+					.filter(row => row.trim().length > 0)
+					.map(row => {
+						return row.replace(/\'/gi, '\\\'');
+					});
+
+				// two cases: it's smaller than the max len, just join as one string
+				let newContent = '';
+				if (lenOfInput < maxLen) {
+					newContent = "IN('" + cleanedUpRows.join("','") + "')";
+				}
+				// the len of input is greate than the max len of elements per IN, we have to split up and create multiple results
+				else {
+					const numberOfEntriesPerRow = avgLineLen > 0 ? 19000 / avgLineLen : 0;
+
+					const results = [];
+					let counter = 0;
+					let itemCounter = 0;
+					let currentLen = 0;
+					let workArray = [];
+
+					while (counter < splitted.length) {
+						workArray[itemCounter] = splitted[counter];
+						currentLen += 3 + workArray[itemCounter].length;
+						itemCounter++;
+						counter++;
+
+						if (currentLen > maxLen) {
+							results.push(workArray);
+							workArray = [];
+							itemCounter = 0;
+							currentLen = 0;
+						}
+					}
+
+					if (workArray.length >0)results.push(workArray);
+
+					for (var i = 0; i < results.length; i++) {
+						newContent += "IN('" + results[i].join("','") + "')" + "\n";
+					}
+
+				}
+
+				editBuilder.replace(selections[idx], newContent);
+			}
+			
+		});
+	});
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
